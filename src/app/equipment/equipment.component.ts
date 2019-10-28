da1a8260717bc6228c7716061a291cbfd7c335c2
Spyryls <yr410ad@gmail.com>
2019-10-28 13:04:48 -0500

    Rebuilding addItem

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-equipment',
@@ -6,26 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+    { name: 'Duct Tape', mass: 0.5 },
+    { name: 'Space Camera', mass: 20 },
+    { name: 'Food', mass: 150 },
+    { name: 'Oxygen Tanks', mass: 400 },
+    { name: 'AE-35 Unit', mass: 5 },
+    { name: 'ISS Supplies', mass: 800 },
+    { name: 'Water', mass: 250 },
+    { name: 'Satellite', mass: 1200 },
+    { name: 'R2 Unit', mass: 32 }
+  ];
 
-   constructor() { }
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
 
-   ngOnInit() { }
+  constructor() { }
 
-   // Code your addItem function here:
-   
+  ngOnInit() { }
+
+  addItem(equipmentLoaded: object): boolean {
+    this.cargoHold.push(equipmentLoaded);
+    this.cargoMass += equipmentLoaded["mass"];
+    if (this.cargoMass <= 1800) {
+      return true;
+    };
+    if (this.cargoHold.length === this.maxItems) {
+      
+    }
+  }
 }
